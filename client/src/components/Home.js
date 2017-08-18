bbe87c95ff31b3ad06d4eb962c9545b53f4fd2e9
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-18 16:32:11 -0600

    set up content file

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { quizcontent } from '../quizcontent';
 const Home = ({}) => {
   return (
     <div>
---
+++ client/src/quizcontent.js
@@ -0,0 +1,26 @@
+import quiz1img from './images/quiz1img.jpeg';
+import quiz2img from './images/quiz2img.jpg';
+
+export const quizcontent = [
+  {
+    key: '01/02/2017',
+    imagesrc: quiz1img,
+    genKnowledge: "What side of the body is this bone from?",
+    genAnswer: "genAnswer",
+    intKnowledge: "This bone forms from the fusion of which 3 bones?",
+    intAnswer: "intAnswer",
+    advKnowledge: "Fusion of these 3 bones generally occurs between what ages?",
+    advAnswer: "intAnswer",
+  },
+  {
+    key: '01/09/2017',
+    imagesrc: quiz2img,
+    genKnowledge: "gen Question",
+    genAnswer: "genAnswer",
+    intKnowledge: "int Question",
+    intAnswer: "intAnswer",
+    advKnowledge: "adv Question",
+    advAnswer: "intAnswer",
+  }
+
+]

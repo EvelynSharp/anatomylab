9d5861f15aad689811e807920e3a8901d9679a1c
Joey Schrader <joedanproperty@gmail.com>
2017-08-18 20:26:27 -0600

    working on quiz

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,12 +1,19 @@
 import React from 'react';
-// import { quizcontent } from '../quizcontent';
-const Quiz = () => {
-  return (
-    <div>
-      Quiz
-    </div>
-  )
+import {Container, Grid, Header, Image} from 'semantic-ui-react';
+import { quizcontent } from '../quizcontent';
 
+
+
+class Quiz extends React.Component {
+
+
+  render(){
+    return (
+      <Container>
+       {this.props.match.params.id}
+      </Container>
+    )
+  }
 }
 
 export default Quiz;
